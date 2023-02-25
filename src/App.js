398e11da78d10f9ac0d4edf8909e561c15b9a6b1
sebast825 <sebamolina825@gmail.com>
2023-02-25 17:00:48 -0300

    funciona api con query

--- src/App.js
+++ src/App.js
@@ -3,14 +3,48 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import Box from './Box';
 
+const clientId = 'BTtUfQ1wl6hb1I3inmzidGfF0qFLvvN71JApPdcu1EQ'
 
 function App() {
-  const [value, setValue] = useState(['https://source.unsplash.com/random/400x400/?creepy'])
+  const [value, setValue] = useState([1, 2, 3, 4])
   const [numberClicked, setNumberClicked] = useState([])
+  const [lvl, setLvl] = useState(1)
+  const [img, setImg] = useState([])
+  const manyBoxes = {
+    1: 4,
+    2: 6,
+    3: 8,
+    4: 10,
+    5: 12,
+    6: 18
+  }
+
+  const apiRequest = () => {
+    //  const endPoint = `https://api.unsplash.com/photos/?client_id=${clientId}&per_page=${manyBoxes[lvl]}&query=mountain&page=${img}`
+     const endPoint = `https://api.unsplash.com/search/photos/?client_id=${clientId}&query=mountain`
+
+    // const endPoint = 'https://api.unsplash.com/search/photos/?client_id=BTtUfQ1wl6hb1I3inmzidGfF0qFLvvN71JApPdcu1EQ&query=mountains&per_page=10&page=2'
+    fetch(endPoint)
+      .then((response) => response.json())
+      .then((jsonData) => {
+        console.log(jsonData)
+        setImg([...jsonData.results])
+      })
+      .catch((eror) => console.log('error: ', eror))
+  }
+
+  useEffect(() => {
+    apiRequest()
+  }, [])
+
+  useEffect(() => {
+    console.log(img)
+  }, [img])
 
   const saveNumber = (e) => {
     isNumberClicked(e)
     setNumberClicked([...numberClicked, e])
+    setLvl(lvl + 1)
   }
 
   const isNumberClicked = (number) => {
@@ -19,12 +53,12 @@ function App() {
 
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     isNextLevel()
 
-  },[numberClicked])
- 
-  const isNextLevel = () =>{
+  }, [numberClicked])
+
+  const isNextLevel = () => {
     const valueLength = value.length;
     const numberClickedLength = numberClicked.length
     console.log(valueLength, numberClickedLength, numberClicked)
@@ -35,20 +69,21 @@ function App() {
   }
 
   const setNextLevel = () => {
-    const addBox = [Number(value.slice(-1)) + 1,Number(value.slice(-1)) +2]
-    setValue([...value,...addBox])
+    const addBox = [Number(value.slice(-1)) + 1, Number(value.slice(-1)) + 2]
+    setValue([...value, ...addBox])
   }
 
   const updateData = () => {
-    const newData = value.sort(() => Math.random() - 0.5)
-    setValue(newData.map(elem => { return elem }))
+    const newData = img.sort(() => Math.random() - 0.5)
+    setImg(img.map(elem => { return elem }))
   }
 
   return (
     <div className="App App-header">
-      {value.map((elem)=>{
+     
+       {img.map((elem)=>{
       return <Box val={elem} updateData={updateData} saveNumber={saveNumber} />
-      })}
+      })} 
     </div>
   );
 }
--- src/Box.js
+++ src/Box.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react'
 
-const clientId = 'BTtUfQ1wl6hb1I3inmzidGfF0qFLvvN71JApPdcu1EQ'
-const endPoint = `https://api.unsplash.com/photos/?client_id=${clientId}`
+
 function Box(props) {
 
   const {val, updateData ,saveNumber} = props;
-
+  
   const [newval, setNewVal] = useState(val)
   useEffect(() => {
     setNewVal(val)
@@ -13,18 +12,12 @@ function Box(props) {
   const [data, setData] = useState(null);
   
   
-    fetch(endPoint)
-      .then((response)=> response.json())
-      .then((jsonData)=>{
-        setNewVal(jsonData[0].urls.small)
-      })
-      .catch((eror)=>console.log('error: ',eror))
 
- 
+
   return (
-    <div className='box' onClick={() => {updateData();saveNumber(val)}}>
+    <div  className='box' onClick={() => {updateData();saveNumber(val)}}>
       {/* <p>{newval}</p> */}
-      <img src={newval}/>
+      <img src={val.urls.small}/>
     </div>
   )
 }
