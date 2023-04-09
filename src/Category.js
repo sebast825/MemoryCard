8b978624fdd9e59a083ca4d2267db1e09024fde0
sebast825 <sebamolina825@gmail.com>
2023-04-08 22:28:42 -0300

    removed comments

--- src/Category.js
+++ src/Category.js
@@ -6,14 +6,6 @@ function Category(props) {
 
   const categories = ['scary', 'art', 'animals'];
 
-  //use the imported images
-  //if you put a photo url here will appear as bg
-  /*const backgroundImageStyles = {
-    scary: "photo",
-    art: "photo"
-
-  };*/
-
   function instructionPopUp() {
     alert("Once the category is selected, click on all photos to pass the level.\nBe careful not to click the same photo twice!");
   }
@@ -25,19 +17,9 @@ function Category(props) {
 
       </div>
 
-
       <div className='categoryContainer'>
         {categories.map((category) => {
 
-          //use the imported images
-        /*  const bgPhoto = {
-            backgroundImage: backgroundImageStyles[category]
-          }*/
-          /*
-          add to category bg to add a bg photo acording to category
-                        style={bgPhoto}
-
-          */
           return (
             <div
               key={category}
