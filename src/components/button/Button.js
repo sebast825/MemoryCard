4d24896763d296d149dbb0e04d3adaa4145072c0
sebast825 <sebamolina825@gmail.com>
2023-04-22 23:33:07 -0300

    button component

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -3,10 +3,10 @@ import './button.scss';
 
 function Button (props) {
    const {clas,setShowTab,view,text} = props;
-   const [newView, setNewView] = useState(view)
+
    return(
       <>
-         <a className={clas} onClick={() => { setShowTab(newView) }}>{text}</a>
+         <a className={clas} onClick={() => { setShowTab(view) }}>{text}</a>
 
       </>
    )
